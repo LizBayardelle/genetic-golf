5fc5968686e03852deadc64b9bb4e90932bef01f
Liz Bayardelle <lizbayardelle@gmail.com>
2016-09-10 17:18:16 -0700

    Assets Precompiled

---
+++ public/assets/ckeditor/config.js
@@ -0,0 +1,6 @@
+CKEDITOR.config.toolbar_mini = [
+      ["Bold","Italic","Underline","Strike"],
+      ["Link", "Unlink"],
+      ["Image", "HorizontalRule"],
+      ["NumberedList","BulletedList","-","Outdent","Indent"],
+  ];
